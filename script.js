6ffafc00b6dbeed57a74ec2965d78cd18b57c989
Harsh Grewal <92568182+harshgrewal27@users.noreply.github.com>
2023-04-14 21:31:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function playMusic(mp3){
+    let audio = new Audio(mp3);
+
+    audio.play();
+}
+
+
+
+
+
+
+document.getElementById("G1").addEventListener('click',() => playMusic("notes/G1.mp3"));
+document.getElementById("G2").addEventListener('click',() => playMusic("notes/G2.mp3"));
+document.getElementById("G3").addEventListener('click',() => playMusic("notes/G3.mp3"));
+document.getElementById("G4").addEventListener('click',() => playMusic("notes/G4.mp3"));
+document.getElementById("G5").addEventListener('click',() => playMusic("notes/G5.mp3"));
+document.getElementById("A1").addEventListener('click',() => playMusic("notes/A1.mp3"));
+document.getElementById("A2").addEventListener('click',() => playMusic("notes/A2.mp3"));
+document.getElementById("A3").addEventListener('click',() => playMusic("notes/A3.mp3"));
+document.getElementById("A4").addEventListener('click',() => playMusic("notes/A4.mp3"));
+document.getElementById("A5").addEventListener('click',() => playMusic("notes/A5.mp3"));
+document.getElementById("A6").addEventListener('click',() => playMusic("notes/A6.mp3"));
+document.getElementById("A7").addEventListener('click',() => playMusic("notes/A7.mp3"));
+document.getElementById("C1").addEventListener('click',() => playMusic("notes/C1.mp3"));
+document.getElementById("C2").addEventListener('click',() => playMusic("notes/C2.mp3"));
+document.getElementById("C3").addEventListener('click',() => playMusic("notes/C3.mp3"));
+document.getElementById("B1").addEventListener('click',() => playMusic("notes/B1.mp3"));
+document.getElementById("D1").addEventListener('click',() => playMusic("notes/D1.mp3"));
+document.getElementById("E1").addEventListener('click',() => playMusic("notes/E1.mp3"));
